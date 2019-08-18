8120755246f5f4fa3006fa2a462ad246255d8944
TorrieL <hmminicity2@163.com>
2019-08-19 00:21:43 +0800

    Mapping Actions & Getters

--- vuex-playground/src/components/ProductListOne.vue
+++ vuex-playground/src/components/ProductListOne.vue
@@ -12,21 +12,29 @@
 </template>
 
 <script>
+  import {mapActions} from 'vuex';
+  import {mapGetters} from 'vuex';
   export default {
     computed: {
       products() {
         return this.$store.state.products
       },
-      saleProducts() {
-        return this.$store.getters.saleProducts
-      }
+      // saleProducts() {
+      //   return this.$store.getters.saleProducts
+      // }
+      ...mapGetters([
+        'saleProducts'
+      ])
     },
     methods: {
-      reducePrice(amount) {
+      //reducePrice(amount) {
         // commit a mutation
         // this.$store.commit('reducePrice')
-        this.$store.dispatch('reducePrice',amount)
-      }
+        //this.$store.dispatch('reducePrice',amount)
+      //}
+      ...mapActions([
+          'reducePrice'
+        ])
     },
   }
 </script>
