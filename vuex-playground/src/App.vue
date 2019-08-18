bbbe5701e3ee1c69d123f23f5af150e0b241a38a
TorrieL <hmminicity2@163.com>
2019-08-18 14:08:15 +0800

    quick dressed

--- vuex-playground/src/App.vue
+++ vuex-playground/src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
-    
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
 </template>
 
 <script>
-
+import ProductListOne from './components/ProductListOne'
+import ProductListTwo from './components/ProductListTwo'
 export default {
   name: 'app',
+  components: {
+    ProductListOne,
+    ProductListTwo,
+  },
   data() {
     return {
-      
+      products: [
+        {name:'Mario Cart',price: 2300},
+        {name:'Luigi Mansion',price: 230000},
+        {name:'Yoshi Sashimi',price: 230},
+        {name:'Toad Mushroom',price: 23}
+      ]
     }
   },
 }
