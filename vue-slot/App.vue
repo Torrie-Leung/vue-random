057e3281f3cfc3a20634a818c2615f0ebc7a9865
TorrieL <hmminicity2@163.com>
2019-08-19 21:40:33 +0800

    vue slots

---
+++ vue-slot/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Helper>
+      <h2 slot="title">{{title}}</h2>
+      <p slot="text">I'm the p-text for the slot</p>
+    </Helper>
+  </div>
+</template>
+
+<script>
+  import Helper from './Helper'
+  export default {
+    components: {
+      Helper,
+    },
+    data() {
+      return {
+        title: 'I\'m a dynamic slot title'
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
