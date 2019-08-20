52a3f4af6127acdc119bed67fdf81f4978c1fcd6
TorrieL <hmminicity2@163.com>
2019-08-20 22:19:16 +0800

    dynamic components examples

---
+++ vue-dynamicComponents/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    
+    <component :is="component"></component>
+  </div>
+</template>
+
+<script>
+  import formOne from './formOne.vue'
+  import formTwo from './formTwo.vue'
+  export default {
+    components: {
+      'form-one':formOne,
+      'form-two':formTwo,
+    },
+    data() {
+      return {
+        component: 'form-two'
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ vue-dynamicComponents/formOne.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form-helper>
+      <div slot="form-header">
+        <h3>Form One - Contact Us</h3>
+        <p>Fill in the form to contact us</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <label>Your Msg</label>
+        <textarea></textarea>
+      </div>
+      <div slot="form-controls">
+        <button @click="handleSubmit">Send</button>
+      </div>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+  import formHelper from './formHelper.vue'
+  export default {
+    components: {
+      'form-helper':formHelper,
+    },
+    methods: {
+      handleSubmit() {
+        alert('thanks for submitting form one & contact us.')
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  div {
+    margin: 0;
+    font-family: sans-serif;
+  }
+</style>
