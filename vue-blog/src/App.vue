61580d82e698926e0ef9f5f9848c9a1feaf33e1e
TorrieL <hmminicity2@163.com>
2019-08-21 13:44:24 +0800

    create a blog

---
+++ vue-blog/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+import ListBlogs from './components/ListBlogs.vue'
+import AddBlog from './components/AddBlog.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'list-blogs':ListBlogs,
+    'add-blog': AddBlog
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0
+}
+</style>
---
+++ vue-blog/src/components/AddBlog.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="add-blog">
+    <h2>Add a new blog post</h2>
+    <form>
+      <label>Blog Title:</label>
+      <input type="text" v-model.lazy.trim="blog.title" required />
+      <label>Blog Content:</label>
+      <textarea v-model.lazy.trim="blog.content"></textarea>
+    </form>
+    <div id="preview">
+      <h3>Preview blog</h3>
+      <p>Blog title: {{blog.title}}</p>
+      <p>Blog content:</p>
+      <p style="white-space:pre">{{blog.content}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        blog: {
+          titile: '',
+          content: ''
+        }
+      }
+    },
+  }
+</script>
+
+<style>
+  #add-blog *{
+    box-sizing: border-box
+  }
+  #add-blog {
+    margin: 20px auto;
+    max-width: 600px
+  }
+  label{
+    display: block;
+    margin: 20px 0 10px;
+  }
+  input[type="text"], textarea{
+    display:block;
+    width: 100%;
+    padding: 8px;
+  }
+  h3 {
+    margin-top: 10px;
+  }
+  #preview{
+    padding: 10px 20px;
+    margin: 30px 0;
+    border: 1px dotted #ccc;
+  }
+</style>
