40199ca171f6a893a8af7858ad93676e18f1bf08
TorrieL <hmminicity2@163.com>
2019-08-22 21:21:56 +0800

    GET request

--- vue-blog/src/App.vue
+++ vue-blog/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <add-blog></add-blog>
+    <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
 import ListBlogs from './components/ListBlogs.vue'
 import AddBlog from './components/AddBlog.vue'
+import ShowBlogs from './components/ShowBlogs.vue'
 
 export default {
   name: 'app',
   components: {
     'list-blogs':ListBlogs,
-    'add-blog': AddBlog
+    'add-blog': AddBlog,
+    'show-blogs': ShowBlogs
   }
 }
 </script>
