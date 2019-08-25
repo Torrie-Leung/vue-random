435bf5d8a93ccfc8d4c81ad31968ee1eb995b1b2
TorrieL <hmminicity2@163.com>
2019-08-25 17:42:35 +0800

    Adding Router Links

--- vue-blog/src/App.vue
+++ vue-blog/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <app-header></app-header>
     <!-- <router-link to="/add">go to addBlog</router-link> -->
     <router-view></router-view>
   </div>
@@ -9,13 +10,15 @@
 import ListBlogs from './components/ListBlogs.vue'
 import AddBlog from './components/AddBlog.vue'
 import ShowBlogs from './components/ShowBlogs.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
     'list-blogs':ListBlogs,
     'add-blog': AddBlog,
-    'show-blogs': ShowBlogs
+    'show-blogs': ShowBlogs,
+    'app-header':Header
   }
 }
 </script>
