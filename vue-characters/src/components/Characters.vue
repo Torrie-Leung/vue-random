4c87e466e02b45735d541c8a5be3f08417ff7153
TorrieL <hmminicity2@163.com>
2019-11-03 22:19:54 +0800

    setup vue-router

---
+++ vue-characters/src/components/Characters.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="characters">
+    characters
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'characters',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ vue-characters/src/routes.js
@@ -0,0 +1,15 @@
+import Characters from './components/Characters.vue'
+import About from './components/About.vue'
+
+export default[
+  {
+    name:'Characters',
+    path: '/',
+    component:Characters
+  },
+  {
+    name:'About',
+    path: '/about',
+    component:About
+  }
+]
