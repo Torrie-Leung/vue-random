ab684ce0357f459d79670986154d07aa36122df3
TorrieL <hmminicity2@163.com>
2019-11-04 21:12:23 +0800

    use axios

--- vue-characters/src/components/Characters.vue
+++ vue-characters/src/components/Characters.vue
@@ -1,14 +1,46 @@
 <template>
-  <div class="characters">
-    characters
+  <div class="characters container">
+    <h1 class="page-header">Wonderland System</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr >
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'characters',
-  props: {
-    msg: String
+  data(){
+    return{
+      characters: []
+    }
+  },
+  created(){
+    this.fetchCharacters()
+  },
+  methods: {
+    fetchCharacters(){
+      this.$http.get("http://localhost:3000/characters")
+          .then((res) => {
+            console.log(res)
+          })
+    }
   }
 }
 </script>
--- vue-characters/src/main.js
+++ vue-characters/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Routes from './routes'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.prototype.$http = axios
 
 const router = new VueRouter({
   routes: Routes,
